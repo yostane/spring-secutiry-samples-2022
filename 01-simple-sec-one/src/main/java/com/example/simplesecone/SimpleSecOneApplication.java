06478e80ab2931fa7e7bf75fe1e3233298964686
yostane <yostane@yahoo.fr>
2022-05-14 19:59:24 +0200

    first commit

---
+++ 01-simple-sec-one/src/main/java/com/example/simplesecone/SimpleSecOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplesecone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSecOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSecOneApplication.class, args);
+	}
+
+}

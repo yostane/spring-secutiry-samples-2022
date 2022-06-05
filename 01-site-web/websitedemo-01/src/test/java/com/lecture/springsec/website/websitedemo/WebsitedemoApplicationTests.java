669d05ea7b67afcfa61cd6d2e05b6a9fb7f4bb1e
yostane <yostane@yahoo.fr>
2022-06-05 11:52:33 +0200

    Added web site sample app

---
+++ 01-site-web/websitedemo-01/src/test/java/com/lecture/springsec/website/websitedemo/WebsitedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecture.springsec.website.websitedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsitedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

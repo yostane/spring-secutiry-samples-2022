fc04ac5908792f0941beeef1220930af98572e4f
yostane <yostane@yahoo.fr>
2022-06-06 22:02:58 +0200

    jwt works

--- 02-rest-api/05-simple-sec-samples-jwt/src/main/java/com/lecture/springsec/security/JwtRequestFilter.java
+++ 02-rest-api/05-simple-sec-samples-jwt/src/main/java/com/lecture/springsec/security/JwtRequestFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,6 +26,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     private JwtTokenUtil jwtTokenUtil;
 
     @Autowired
+    @Lazy
     UserDetailsService userDetailsService;
 
     @Override
--- 02-rest-api/05-simple-sec-samples-jwt/src/main/java/com/lecture/springsec/security/WebSecurityConfig.java
+++ 02-rest-api/05-simple-sec-samples-jwt/src/main/java/com/lecture/springsec/security/WebSecurityConfig.java
@@ -4,15 +4,24 @@ import javax.servlet.ServletException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -21,29 +30,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
 
+    @Autowired
+    JwtRequestFilter jwtRequestFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().httpBasic()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/protectedGreeting", "/users/**").hasAnyRole("USER", "ADMIN")
-                .and().csrf().disable();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // bcrypt of 'password':
-        // $2a$10$lHamYXOcefYUwmPsMpyXBe5HKiXuxnt0rt38YJNpa9KGQA2kzTXBa
-        auth.inMemoryAuthentication().withUser("user")
-                .password("$2a$10$lHamYXOcefYUwmPsMpyXBe5HKiXuxnt0rt38YJNpa9KGQA2kzTXBa").roles("USER")
-                .and().withUser("admin").password("lHamYXOcefYUwmPsMpyXBe5HKiXuxnt0rt38YJNpa9KGQA2kzTXBa")
-                .roles("ADMIN", "USER");
+                .antMatchers("/protectedGreeting", "/users/**").authenticated()
+                .and()
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable();
     }
 
     @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    // https://stackoverflow.com/a/21639553
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
